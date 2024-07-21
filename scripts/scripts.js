5dc73829f463b9b48ee0b7cdb5a719ea87679a13
Yash Raj <89970907+yash-raj202134@users.noreply.github.com>
2024-07-21 14:32:54 +0530

    Menu update f(ixed on top)

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,3 +11,29 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
   
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".navbar").style.top = "0";
+  } else {
+    document.querySelector(".navbar").style.top = "-60px"; // Adjust this value based on your navbar height
+  }
+  prevScrollpos = currentScrollPos;
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+    const navbarHeight = document.querySelector('.navbar').offsetHeight;
+    
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop - navbarHeight,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
