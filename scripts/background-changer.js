cc3909a7e1956ec9c4c115b0fce6c4948ce77115
Yash Raj <89970907+yash-raj202134@users.noreply.github.com>
2024-08-11 21:18:47 +0530

    backgrounfd images update

--- scripts/background-changer.js
+++ scripts/background-changer.js
@@ -1,9 +1,11 @@
 const backgrounds = [
-    'images/home-background1.jpg',
-    'images/home-background2.jpg',
-    'images/home-background3.jpg',
-    'images/home-background4.jpg',
-    'images/home-background5.jpg'
+    'images/home-background/home-background1.jpg',
+    'images/home-background/home-background2.jpg',
+    'images/home-background/home-background3.jpg',
+    'images/home-background/home-background4.jpg',
+    'images/home-background/home-background5.jpg',
+    'images/home-background/home-background6.jpg'
+
 ];
 
 let currentIndex = 0;
@@ -24,5 +26,5 @@ backgrounds.forEach(src => {
     img.src = src;
 });
 
-// Change background every 10 seconds
-setInterval(changeBackground, 10000);
+// Change background every 10 seconds x 6 = 60 seconds = 1 minutes
+setInterval(changeBackground, 10000*6);
